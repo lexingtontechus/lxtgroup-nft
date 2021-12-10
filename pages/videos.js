de4138f7782d6f83c4526d210928b8b550a6e078
Aloysius Teo <aloysiusteo72@gmail.com>
2021-12-09 17:25:55 -0800

    Customizations

--- pages/videos.js
+++ pages/videos.js
@@ -1,19 +1,6 @@
 //https://link.ap1.storjshare.io/s/jvmji4ggxbqzlmltr2zxqkgsrntq/lxtgroup/foodtech_biotech.mp4
 //https://link.ap1.storjshare.io/s/jvmji4ggxbqzlmltr2zxqkgsrntq/lxtgroup/foodtech_biotech.mp4?wrap=0"
 
-<ReactPlayer
-  url="https://link.ap1.storjshare.io/s/jvmji4ggxbqzlmltr2zxqkgsrntq/lxtgroup/foodtech_biotech.mp4?wrap=0"
-  controls
-  playing={isPlaying}
-  config={{
-    file: {
-      attributes: {
-        controlsList: "nodownload"
-      }
-    }
-  }}
-/>;
-
 //https://link.ap1.storjshare.io/s/jwylcpaqzdib5uqg32lltvzl527a/lxtgroup/quantum_management.mp4?wrap=0
 
 //https://link.ap1.storjshare.io/s/jvwmx4khbal5hdno43j5jo73yruq/lxtgroup/etf.mp4?wrap=0
