21f70f8a3648388826f192291058843f266f672c
Aloysius Teo <aloysiusteo72@gmail.com>
2021-12-30 15:07:50 -0800

    initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GTM_ID } from '../lib/gtm';
 
 class MyDocument extends Document {
   render() {
@@ -47,6 +48,15 @@ Crypto EFT & Hyperlocal Microservices.  Web 3.0 Blockchain domain IPFS "
 
         </Head>
         <body className="text-stone-700 antialiased">
+        <noscript>
+            <iframe
+              title="Tag Manager"
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <div id="page-transition"></div>
           <Main />
           <NextScript />
