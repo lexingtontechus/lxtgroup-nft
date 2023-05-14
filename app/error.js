2e4805ec5f70c9fdacf5b9b984961955c9bae4cf
Aloysius Teo <aloysiusteo72@gmail.com>
2023-05-14 22:33:11 +0000

    Update 3 files

--- app/error.js
+++ app/error.js
@@ -1,12 +1,25 @@
-import React, { Component } from "react";
-import Router from "next/router";
-
-export default class _error extends Component {
-  componentDidMount = () => {
-    Router.push("/");
-  };
-
-  render() {
-    return <div />;
-  }
+'use client'; // Error components must be Client Components
+ 
+import { useEffect } from 'react';
+ 
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+ 
+  return (
+    <main>
+      <h2 className="pb-4 text-center text-4xl font-bold uppercase text-primary-50">AI Gone Wrong!</h2>
+      <button
+       className="pb-4 text-center text-4xl font-bold uppercase text-primary-100 bg-secondary-400" 
+       onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Restart AI
+      </button>
+    </main>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ const Home = () => {
           <div className="flex flex-wrap items-center">
             <div className="ml-auto mr-auto w-full items-center justify-center px-4 pt-24 text-center lg:w-10/12">
               <div className="p-2">
-                <h1 className="pb-4 text-center text-4xl font-bold uppercase text-primary-100">
+                <h1 className="pb-4 text-center text-4xl font-bold uppercase text-primary-50">
                   Quantum Management
                 </h1>
                 {/*<div className="w-full h-4/12">PLAYER</div>*/}
